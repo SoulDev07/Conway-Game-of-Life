c5e88e2c926e9dd0afc0b0c31914f5258907c4ba
SoulDev07 <mayankravariya@gmail.com>
2024-01-30 21:44:06 +0530

    Add game logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,65 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from '@/app/page.module.css'
 
 const numRows = 15;
 const numCols = 30;
 
+const Cell = ({ isAlive, onClick }) => (
+  <div className={styles.cell} style={{ backgroundColor: isAlive ? 'grey' : 'white' }} onClick={onClick} />
+);
+
+
 function generateBoard() {
-  const rows = [];
+  let board = [];
   for (let i = 0; i < numRows; i++)
-    rows.push(Array(numCols).fill(0));
-  return rows;
+    board.push(Array(numCols).fill(false));
+  return board;
 }
 
 export default function Home() {
-  const [board, setBoard] = useState(generateBoard);
+  const [board, setBoard] = useState(() => generateBoard());
+
+  useEffect(() => {
+    const interval = setInterval(updateBoard, 500);
+    return () => clearInterval(interval);
+  }, [updateBoard]);
+
+  function toggleCell(row, col) {
+    const newBoard = [...board];
+    newBoard[row][col] = !newBoard[row][col];
+    setBoard(newBoard);
+  };
+
+  function updateBoard() {
+    const newBoard = board.map((row, i) => row.map((cell, j) => updateCell(i, j)));
+    setBoard(newBoard);
+  }
+
+  function updateCell(i, j) {
+    const neighbours = countNeighbors(i, j);
+
+    if (board[i][j])
+      return (neighbours <= 1 || neighbours >= 4) ? false : true;
+    else
+      if (neighbours === 3)
+        return true;
+  }
+
+  function countNeighbors(i, j) {
+    let count = 0;
+    const neighborsOffsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+
+    neighborsOffsets.forEach(([offsetI, offsetJ]) => {
+      const newRow = i + offsetI;
+      const newCol = j + offsetJ;
+
+      if (newRow >= 0 && newRow < numRows && newCol >= 0 && newCol < numCols && board[newRow][newCol])
+        count++;
+    });
+
+    return count;
+  }
 
   return (
     <main className={styles.mainContainer}>
@@ -22,7 +68,7 @@ export default function Home() {
       <div className={styles.gameContainer} style={{ gridTemplateColumns: `repeat(${numCols}, var(--cell-size))` }}>
         {board.map((row, i) =>
           row.map((cell, j) =>
-            <div key={(i, j)} className={styles.cell} />
+            <Cell key={`${i}-${j}`} isAlive={board[i][j]} onClick={() => toggleCell(i, j)} />
           )
         )}
       </div>
